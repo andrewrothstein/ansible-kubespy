6082b9f990bfbab78a61903a5e79a1c451da7d96
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-10-19 00:20:19 -0400

    do over at v0.6.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -5,32 +5,35 @@ MIRROR=https://github.com/pulumi/kubespy/releases/download
 
 dl()
 {
-    local ver=$1
-    local os=$2
-    local arch=$3
-    local archive_type=${4:-tar.gz}
+    local lchecksums=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=$5
+    local archive_type=tar.gz
     local platform="$os-$arch"
-    local url=$MIRROR/$ver/kubespy-$platform.$archive_type
-    local lfile=$DIR/kubespy-$ver-$platform.$archive_type
-
-    if [ ! -e $lfile ];
-    then
-        wget -q -O $lfile $url
-    fi
+    local file="kubespy${dotexe}-${ver}-${platform}.${archive_type}"
+    local url="${MIRROR}/${ver}/$file"
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
 }
 
 dlver () {
     local ver=$1
+    # https://github.com/pulumi/kubespy/releases/download/v0.6.0/checksums.txt
+    local rchecksums="${MIRROR}/${ver}/checksums.txt"
+    local lchecksums="${DIR}/kubespy-${ver}-checksums.txt"
+
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $rchecksums
+    fi
+    printf "  # %s\n" $rchecksums
     printf "  %s:\n" $ver
-    dl $ver darwin 386
-    dl $ver darwin amd64
-    dl $ver linux 386
-    dl $ver linux amd64
-    dl $ver windows 386
-    dl $ver windows amd64
+    dl $lchecksums $ver darwin amd64
+    dl $lchecksums $ver linux amd64
+    dl $lchecksums $ver windows amd64 .exe
 }
 
-dlver ${1:-v0.5.1}
+dlver ${1:-v0.6.0}
