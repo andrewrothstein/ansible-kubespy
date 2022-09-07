0d4a6873be5a73e3cae32e2242c5efe6f9e7bc43
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-09-07 12:57:32 -0400

    kubespy:0.6.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -12,8 +12,8 @@ dl()
     local dotexe=$5
     local archive_type=tar.gz
     local platform="$os-$arch"
-    local file="kubespy${dotexe}-${ver}-${platform}.${archive_type}"
-    local url="${MIRROR}/${ver}/$file"
+    local file="kubespy-v${ver}-${platform}.${archive_type}"
+    local url="${MIRROR}/v${ver}/$file"
 
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
@@ -22,18 +22,18 @@ dl()
 dlver () {
     local ver=$1
     # https://github.com/pulumi/kubespy/releases/download/v0.6.0/checksums.txt
-    local rchecksums="${MIRROR}/${ver}/checksums.txt"
+    local rchecksums="${MIRROR}/v${ver}/checksums.txt"
     local lchecksums="${DIR}/kubespy-${ver}-checksums.txt"
 
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $rchecksums
+        curl -sSLf -o $lchecksums $rchecksums
     fi
     printf "  # %s\n" $rchecksums
-    printf "  %s:\n" $ver
+    printf "  '%s':\n" $ver
     dl $lchecksums $ver darwin amd64
     dl $lchecksums $ver linux amd64
-    dl $lchecksums $ver windows amd64 .exe
+    dl $lchecksums $ver windows amd64
 }
 
-dlver ${1:-v0.6.0}
+dlver ${1:-0.6.1}
